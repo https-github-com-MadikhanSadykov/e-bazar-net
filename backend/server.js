6bb5f46f6ee72a02c0b46b3c48a6429a9de2624b
Madikhan Sadykov <madikhansadykov@gmail.com>
2022-05-05 18:29:39 +0600

    Connect to Mongo DB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,27 @@
-const express = require("express");
-const cors = require("cors");
-const data = require("./data.js");
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import data from './data.js';
+import userRouter from './routers/userRouter.js';
+
+
+const MONGODB_URL = "mongodb://localhost:27017";
+
+mongoose
+  .connect(MONGODB_URL, {
+  
+  })
+  .then(() => {
+    console.log('Connected to mongodb.');
+  })
+  .catch((error) => {
+    console.log(error.reason);
+  });
+  
 const app = express();
 
 app.use(cors());
+app.use('/api/users', userRouter);
 app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,7 +4,7 @@ import { apiUrl } from "./config";
 export const getProduct = async (id) => {
   try {
     const response = await axios({
-      url: apiUrl + "/api/products/" + id,
+      url: `${apiUrl  }/api/products/${  id}`,
       method: "GET",
       headers: {
         "Content-Type": "application/json",
