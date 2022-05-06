a4cc4d6cff744f012aea0387b381cb5f12e6c4a7
Madikhan Sadykov <madikhansadykov@gmail.com>
2022-05-06 12:41:29 +0600

    BACKEND SignIn

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 import data from './data.js';
+import config from './config.js';
 import userRouter from './routers/userRouter.js';
 
-
-const MONGODB_URL = "mongodb://localhost:27017";
-
 mongoose
-  .connect(MONGODB_URL, {
-  
+  .connect(config.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
   .then(() => {
     console.log('Connected to mongodb.');
@@ -17,24 +17,25 @@ mongoose
   .catch((error) => {
     console.log(error.reason);
   });
-  
 const app = express();
-
 app.use(cors());
+app.use(bodyParser.json());
 app.use('/api/users', userRouter);
-app.get("/api/products", (req, res) => {
+app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
-
-app.get("/api/products/:id", (req, res) => {
+app.get('/api/products/:id', (req, res) => {
   const product = data.products.find((x) => x._id === req.params.id);
   if (product) {
     res.send(product);
   } else {
-    res.status(404).send({ message: "Product Not Found!" });
+    res.status(404).send({ message: 'Product Not Found!' });
   }
 });
-
+app.use((err, req, res, next) => {
+  const status = err.name && err.name === 'ValidationError' ? 400 : 500;
+  res.status(status).send({ message: err.message });
+});
 app.listen(5000, () => {
-  console.log("serve at http://localhost:5000");
+  console.log('serve at http://localhost:5000');
 });
