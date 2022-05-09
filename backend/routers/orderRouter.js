0dce41d406360521807c114c3d90b3d14f7408a3
Madikhan Sadykov <madikhansadykov@gmail.com>
2022-05-09 07:45:41 +0600

    Admin Dashboard Summury Frontend

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -2,8 +2,35 @@ import express from 'express';
 import expressAsyncHandler from 'express-async-handler';
 import { isAuth, isAdmin } from '../utils.js';
 import Order from '../models/orderModel.js';
+import User from '../models/userModel.js';
 
 const orderRouter = express.Router();
+
+orderRouter.get(
+  '/summary',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const orders = await Order.aggregate([
+      {
+        $group: {
+          _id: null,
+          numOrders: { $sum: 1 },
+          totalSales: { $sum: '$totalPrice' },
+        },
+      },
+    ]);
+    const users = await User.aggregate([
+      {
+        $group: {
+          _id: null,
+          numUsers: { $sum: 1 },
+        },
+      },
+    ]);
+    res.send({ users, orders });
+  })
+);
 
 orderRouter.get(
   '/',
@@ -101,4 +128,5 @@ orderRouter.put(
     }
   })
 );
+
 export default orderRouter;
